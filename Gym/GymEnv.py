1aab10b572029c946a447beb2dfb10926a3ca8fd
IMM RL Lab <53929619+imm-rl-lab@users.noreply.github.com>
2022-08-24 20:35:39 +0500

    Update GymEnv.py

--- Gym/GymEnv.py
+++ Gym/GymEnv.py
@@ -1,7 +1,10 @@
 import gym, os
 import numpy as np
 import pathlib
-
+import os
+os.add_dll_directory(os.path.join(os.path.expanduser('~'), ".mujoco", "mjpro150", "bin"))
+from mujoco_py import GlfwContext
+GlfwContext(True)
 
 class NPArrayWithPhysics(np.ndarray):
     def __new__(cls, input_array, physics=None):
@@ -13,10 +16,8 @@ class NPArrayWithPhysics(np.ndarray):
         if obj is None: return
         self.physics = getattr(obj, 'physics', None)
 
-
-
 class GymEnv:
-    def __init__(self, domain_name):
+    def __init__(self, domain_name, dt):
         self.domain_name = domain_name
         
         if domain_name in ['Ant-v3', 'HalfCheetah-v3', 'Hopper-v3', 'Humanoid-v3', 
@@ -24,12 +25,23 @@ class GymEnv:
             self.env = gym.make(domain_name, reset_noise_scale=0)
         else:
             self.env = gym.make(domain_name)
+        
         self.env.seed(21)
         self.state_dim = self.env.observation_space.shape[0]
         self.action_dim = self.env.action_space.shape[0]
         self.action_min = self.env.action_space.low
         self.action_max = self.env.action_space.high
+        self.max_episode_steps = self.env._max_episode_steps
+        
         self.pos_dim = {'InvertedDoublePendulum-v2':3, 'InvertedPendulum-v2':2}
+        
+        self.inner_dt = self.env.dt / self.env.env.frame_skip
+        self.env.env.frame_skip = max(1, int(dt / self.inner_dt + 1e-6))
+        self.dt = self.env.env.frame_skip * self.inner_dt
+        
+        if self.dt != dt:
+            print('Warning: the closest possible dt is ', self.dt)
+        
         return None
         
     def reset(self):
@@ -43,7 +55,6 @@ class GymEnv:
             state = NPArrayWithPhysics(state, physics=sim_state)
             self.env.sim.set_state(state.physics)
             return state
-            
         else:
             state = self.env.reset()
             state = NPArrayWithPhysics(state, self.env.sim.get_state())
@@ -55,6 +66,7 @@ class GymEnv:
         return state, reward, done, info
     
     def virtual_step(self, state, action):
+        self.reset()
         self.env.sim.set_state(state.physics)
         state, reward, done, info = self.env.step(action)
         state = NPArrayWithPhysics(state, self.env.sim.get_state())
