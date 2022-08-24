6d0210b23001ca41a5db569aedce608d38641e75
IMM RL Lab <53929619+imm-rl-lab@users.noreply.github.com>
2022-08-24 16:00:06 +0500

    Update DMControlEnvWithPhysics.py

--- DMControl/DMControlEnvWithPhysics.py
+++ DMControl/DMControlEnvWithPhysics.py
@@ -1,6 +1,13 @@
+import os, sys
 import numpy as np
 import copy
 from dm_control import suite
+sys.path.insert(0, os.path.abspath('..'))
+from DMControl.DMControlEnv import DMControlEnv
+
+PREFIXES = ['efc_AR','D_','efc_J_', 'efc_JT', 'efc_vel', 'efc_state', 'efc_margin', 
+            'efc_frictionloss', 'efc_b','efc_force', 'cfrc_ext', 'cfrc_int', 'qDeriv', 
+            'qLU', 'xaxis', 'xfrc_applied', 'actuator_moment', 'cacc', 'cam_xmat']
 
 
 class NPArrayWithPhysics(np.ndarray):
@@ -14,42 +21,61 @@ class NPArrayWithPhysics(np.ndarray):
         self.physics = getattr(obj, 'physics', None)
 
 
-class DMControlEnvWithPhysics():
-    def __init__(self, domain_name, task_name):
-        self.env = suite.load(domain_name, task_name, task_kwargs={'random': np.random.RandomState(21)})
-        self.state_dim = self.env.physics.state().shape[0]
-        self.action_dim = self.env.action_spec().shape[0]
-        self.action_min = self.env.action_spec().minimum
-        self.action_max = self.env.action_spec().maximum
-        self.attrs = []
-        for attr_name in dir(self.env.physics.data):
-            attr = getattr(self.env.physics.data, attr_name)
-            if type(attr) is np.ndarray or type(attr) is int or type(attr) is float:
-                self.attrs.append(attr_name)
-        
+class DMControlEnvWithPhysics(DMControlEnv):
+    def __init__(self, domain_name, task_name, dt):
+        super().__init__(domain_name, task_name, dt)
+        self.domain_name = domain_name
+        self.attrs = self.get_attributes()
         return None
-        
+    
     def reset(self):
-        self.env.reset()
-        self.env.physics.reset()
-        state = NPArrayWithPhysics(self.env.physics.state(), physics=self.get_physics())
+        state = self._reset()
+        state = NPArrayWithPhysics(state, physics=self.get_physics())
         self.state = state
         return self.state
     
     def step(self, action):
-        step = self.env.step(action)
-        state = NPArrayWithPhysics(self.env.physics.state(), physics=self.get_physics())
+        state, reward, done, info = self._step(action)
+        state = NPArrayWithPhysics(state, physics=self.get_physics())
         self.state = state
-        return state, step.reward, step.last(), {}
+        return self.state, reward, done, info
     
     def virtual_step(self, state, action):
+        self.reset()
         self.env.physics.set_state(state)
-        for attr_name in self.attrs:
-            setattr(self.env.physics.data, attr_name, state.physics[attr_name])
-                
-        step = self.env.step(action)
-        state = NPArrayWithPhysics(self.env.physics.state(), physics=self.get_physics())
-        return state, step.reward, step.last(), {}
+        self.set_physics(state)
+        state, reward, done, info = self._step(action)
+        state = NPArrayWithPhysics(state, physics=self.get_physics())
+        return state, reward, done, info
+    
+    def get_partial_attributes(self):
+        #attrs = []
+        #for attr_name in dir(self.env.physics.data):
+        #   attr = getattr(self.env.physics.data, attr_name)
+        #    if type(attr) is np.ndarray or type(attr) is int or type(attr) is float:
+        #        attrs.append(attr_name)
+        attrs = []
+        for attr_name in dir(self.env.physics.data):
+            ok = False
+            for prefix in ['actuator_']:
+                if prefix in attr_name:
+                    ok = True
+            if ok:
+                attrs.append(attr_name)
+            
+        return attrs
+    
+    def get_attributes(self):
+        attrs = []
+        for attr_name in dir(self.env.physics.data):
+            attr = getattr(self.env.physics.data, attr_name)
+            if type(attr) is np.ndarray or type(attr) is int or type(attr) is float:
+                if self.domain_name in ['finger', 'manipulator']:
+                    attrs.append(attr_name)
+                else:
+                    if not any(prefix in attr_name for prefix in PREFIXES):
+                        attrs.append(attr_name)
+        return attrs
     
     def get_physics(self):
         physics = {}
@@ -57,3 +83,8 @@ class DMControlEnvWithPhysics():
             attr = copy.copy(getattr(self.env.physics.data, attr_name))
             physics[attr_name] = attr
         return physics
+    
+    def set_physics(self, state):
+        for attr_name in self.attrs:
+            setattr(self.env.physics.data, attr_name, state.physics[attr_name])
+        return None
